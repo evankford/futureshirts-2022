d4d43d9f136918923ec14eea7684fdfdc35f1887
Evan Ford <evankerrickford@gmail.com>
2022-09-10 18:36:12 -0400

    Vite safe env variables

--- src/routes/upload/[fileName]/+server.ts
+++ src/routes/upload/[fileName]/+server.ts
@@ -6,8 +6,8 @@ import { json, error } from '@sveltejs/kit';
 const uploader = new S3Client({
     region: 'us-east-1',
     credentials: {
-      accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+      accessKeyId:import.meta.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey:import.meta.env.AWS_SECRET_ACCESS_KEY
     }
 })
 export const POST:RequestHandler = async({request, params})=> {
