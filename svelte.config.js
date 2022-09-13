ba8d239b3d132b77379ec7bd29b36dc811be50ae
Evan Ford <evankerrickford@gmail.com>
2022-09-12 20:29:02 -0400

    Netlify Now

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true,
+			split: false,
+		}),
 	}
 };
 
