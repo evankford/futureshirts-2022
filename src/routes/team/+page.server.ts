3cce7d923e9f817fe8a85794e22144c184d345fc
Evan Ford <evankerrickford@gmail.com>
2023-01-06 13:31:38 -0500

    Updated Fa-Svelte

--- src/routes/team/+page.server.ts
+++ src/routes/team/+page.server.ts
@@ -4,7 +4,6 @@ import type { TeamPageData} from "$lib/types/sanity";
 import type { PageServerLoad } from "./$types";
 const load:PageServerLoad= async()=>{
     const query = `${getSingleDocumentFromSlug('team')}{ title, subtitle, 'teamMembers': ${getTeamMembers()}{title, image, questions}}`;
-    console.log(query);
     return await sanityGet<TeamPageData>(query);
 }
 export {load};
