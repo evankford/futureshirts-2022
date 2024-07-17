ffcfeed1b00bd445f07ebb3729d0ac32843d61d7
Evan Ford <evankerrickford@gmail.com>
2024-07-17 13:18:55 -0400

    Just use Rest API

--- src/routes/(site)/sendemail/+server.ts
+++ src/routes/(site)/sendemail/+server.ts
@@ -1,9 +1,8 @@
 
-import * as ElasticEmail from '@elasticemail/elasticemail-client';
 import {  json } from '@sveltejs/kit';
 import {contact, job, support} from '$lib/emailTemplate';
 import type { RequestHandler } from "./$types";
-import type {ContactData, JobData, ResponseError, SupportData} from "../../global";
+import type {ContactData, JobData, ResponseError, SupportData} from "../../../global";
 
 function generateHTML(data: ContactData | JobData | SupportData):string  {
   if (!('formName' in data)) {
@@ -22,18 +21,13 @@ function generateHTML(data: ContactData | JobData | SupportData):string  {
 }
 
 export const POST:RequestHandler = async ({ request }) => {
-  const defaultClient = ElasticEmail.ApiClient.instance;
-  const apikey = defaultClient.authentications['apikey'];
-  apikey.apiKey = import.meta.env.ELASTIC_EMAIL_APIKEY
-  const api = new ElasticEmail.EmailsApi()
-
   const errors: ResponseError[] = [];
 
-  const sentFormData:JobData|ContactData = await request.json();
+  const sentFormData: JobData|ContactData = await request.json();
 
   const html = generateHTML(sentFormData);
 
-  const emailData = {
+  const body = {
     Recipients: {
       To: ['evan.ford@futureshirts.com'],
       // To: sentFormData.emailTo,
@@ -47,30 +41,26 @@ export const POST:RequestHandler = async ({ request }) => {
         }
       ],
       ReplyTo: sentFormData.email,
-      From: "contact@futureshirts.com",
+      From: "website@futureshirts.com",
       Subject: `${sentFormData.formName} Submission ${'topic' in sentFormData ? '(' + sentFormData.topic + ')' : ''}`,
     },
   }
 
+  const headers = {
+    'X-ElasticEmail-ApiKey' : import.meta.env.ELASTIC_EMAIL_APIKEY,
+    'Content-Type': 'application/json'
+  }
+
+  const url = 'https://api.elasticemail.com/v4/emails/transactional'
+
 
     try {
-      let data = null
-      let init = {status: 200}
-      const handler = function(error, data, response) {
-        if (error) {
-          console.error(error)
-          errors.push({code: 500, message: 'Could not send message. Check your logs for more info.'})
-          data = { errors }
-          init =  {status: 520}
-          return
-        }
-        data = {
-          message: 'Successfully sent email',
-          errors
-        }
-      }
-      api.emailsTransactionalPost(emailData, handler)
-      return json(data, init)
+      const resp = await fetch(url, {
+        body: JSON.stringify(body),
+        method: 'POST',
+        headers,
+      })
+      return json({message: 'Your email is sent!'}, {status: 200})
     } catch (error) {
       console.error(error)
       errors.push({code: 500, message: 'Could not send message. Check your logs for more info.'})
